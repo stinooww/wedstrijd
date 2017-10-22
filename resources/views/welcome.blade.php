7ba32815a011879ff7acce9d7f20ab5a1126702d
stijn heynderickx <stijn.heynderickx@gmail.com>
2017-10-23 00:00:36 +0200

    hello world

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -54,7 +54,6 @@
                 padding: 0 25px;
                 font-size: 12px;
                 font-weight: 600;
-                letter-spacing: .1rem;
                 text-decoration: none;
                 text-transform: uppercase;
             }
