39f64fbb01c343975ee273cf868182740b7a651d
stijn heynderickx <stijn.heynderickx@gmail.com>
2017-10-22 23:57:35 +0200

    hello world

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,20 +79,14 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Yoooooooooooooooooooooooooooooooooooo
+                    Yoooooooooo
 
 
 
 
                 </div>
 
-                <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
+
             </div>
         </div>
     </body>
