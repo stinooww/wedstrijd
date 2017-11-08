749d7821b20c5552054cb30b94f8d86bd47efb44
stijn heynderickx <stijn.heynderickx@gmail.com>
2017-11-08 11:16:00 +0100

    wedstrijd controller fix/ cron command

--- app/Console/Commands/SelectWinner.php
+++ app/Console/Commands/SelectWinner.php
@@ -22,7 +22,7 @@ class SelectWinner extends Command
      *
      * @var string
      */
-    protected $description = 'Selecteert een winnende deelnemer';
+    protected $description = 'Selecteert een winnende deelnemer en start volgende spel';
 
     /**
      * Create a new command instance.
@@ -39,7 +39,8 @@ class SelectWinner extends Command
      *
      * @return mixed
      */
-    //selecteert een random deelnemer met het juiste antwoord en slaagt die op in winnaar
+
+    //update het huidige spel en stelt een nieuwe in
     public function handle()
     {
         $date = Carbon::now();
@@ -47,19 +48,44 @@ class SelectWinner extends Command
         $activeGame = Wedstrijd::where('is_active', 1)->get()->first();
         $activeGame = $activeGame->id;
         $this->info($activeGame);
-        $randomwinnaar = Deelnemer::where([
-            ['question', 20],
-            ['wedstrijd_id', $activeGame],
-        ])->orderByRaw("RAND()")->get()->first();
 
-        $winnaarID = $randomwinnaar->id;
-        $this->info($winnaarID);
+        if ($activeGame->end_date == $date) {
+            $randomwinnaar = Deelnemer::where([
+                ['question', 20],
+                ['wedstrijd_id', $activeGame],
+            ])->orderByRaw("RAND()")->get()->first();
 
-        $winnaar = new Winnaar();
-        $winnaar->deelnemer_id = $winnaarID;
-        $winnaar->save();
-        $this->info($winnaar);
-        return app('App\Http\Controllers\ParticipantController')->SendWinningMail();
+            $winnaarID = $randomwinnaar->id;
+            $this->info("de winnaar met id " . $winnaarID);
+
+            $winnaar = new Winnaar();
+            $winnaar->deelnemer_id = $winnaarID;
+            $winnaar->save();
+            $this->info($winnaar);
+            $this->nextGame();
+            return app('App\Http\Controllers\ParticipantController')->SendWinningMail();
+        } else {
+            $this->info('eind datum van actieve wedstrijd is nog niet vandaag');
+        }
+
 
     }
+
+
+
+
+    //checkt eerst of er een actieve wedstrijd is, selecteert een random deelnemer met het juiste antwoord,
+    // stelt een nieuw spel in  en slaagt die vervolgens op in winnaar + stuurt een mail
+
+
+    public function nextGame()
+    {
+        $date = Carbon::now();
+        Wedstrijd::where('end_date', $date)->update(['is_active' => 0]);
+        Wedstrijd::where('start_date', $date)->update(['is_active' => 1]);
+        $activeGame = Wedstrijd::where('is_active', 1)->get()->first();
+        $activeGame = $activeGame->id;
+        $this->info($activeGame);
+    }
+
 }
